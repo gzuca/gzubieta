78befd51f9bd517fba1d2d118e0c256129993d07
gzuca <gzubica@gmail.com>
2025-08-03 17:29:47 -0500

    prueba con imagenes

--- src/data/skills.js
+++ src/data/skills.js
@@ -2,46 +2,46 @@ export const skills = [
     {
         title:"HTML",
         description:"HTML (HyperText Markup Language) is the standard for structuring content on the web. It allows creating pages with elements like headings, paragraphs, lists, and images.",
-        image:'/public/html.png',
+        image:'/html.png',
     },
 
     {
         title:"CSS",
         description:"CSS (Cascading Style Sheets) is used to style web pages, controlling colors, fonts, layouts, and animations to enhance appearance and usability.",
-        image:"/public/css.png",
+        image:"/css.png",
     },
 
     {
         title:"JavaScript",
         description:"JavaScript is a programming language that adds interactivity to web pages, enabling dynamic features like forms, animations, and web applications.",
-        image:"/public/javascript.png",
+        image:"/javascript.png",
     },
 
     {
         title:"React",
         description:"React is a JavaScript library for building interactive and reusable user interfaces, based on components, widely used in modern applications.",
-        image:"/public/react.png",
+        image:"/react.png",
     },
 
     {
         title:"Adobe Illustrator",
         description:"Adobe Illustrator is a vector design tool used to create logos, illustrations, and custom graphics with high precision.",
-        image:"/public/adobei.png",
+        image:"/adobei.png",
     },
     {
         title:"Figma",
         description:"Figma is a cloud-based collaborative design tool, ideal for prototyping and designing user interfaces with a focus on UX/UI.",
-        image:"/public/figma.png",
+        image:"/figma.png",
     },
     {
         title:"Git",
         description:"Git is a version control system that allows managing and collaborating on software development projects, tracking code changes.",
-        image:"/public/git.png",
+        image:"/git.png",
     },
     {
         title:"UX/UI",
         description:"UX/UI Design focuses on creating intuitive and visually appealing user experiences, combining research, interaction design, and aesthetics.",
-        image:"/public/uxui.png",
+        image:"/uxui.png",
     },
 
 ];
