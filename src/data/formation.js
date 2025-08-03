4f51bf54f62fcd283665afc2c55b607cf6ff3410
gzuca <gzubica@gmail.com>
2025-08-03 17:33:12 -0500

    actualizacion para imagenes

--- src/data/formation.js
+++ src/data/formation.js
@@ -2,26 +2,26 @@ export const formations =[
     {
         title:"Full-Stack Engineer (currently studying)",
         institution:"Codecademy",
-        image:"/public/Comingsoon.jpg",
+        image:"/Comingsoon.jpg",
     },
     {
         title:"Front-End Development Professional Certificate",
         institution:"Meta - Coursera",
-        image:"/public/meta-front-end-developer.png",
+        image:"/meta-front-end-developer.png",
     },
     {
         title:"UX/UI Design Professional Certificate",
         institution:"Google - Coursera",
-        image:"/public/google-ux-design.png",
+        image:"/google-ux-design.png",
     },
     {
         title:"Project Management Professional Certificate",
         institution:"Google - Coursera",
-        image:"/public/google-project-management.png",
+        image:"/google-project-management.png",
     },
     {
         title:"Industrial Engieneer",
         institution:"Universidad el bosque",
-        image:"/public/degree.png",
+        image:"/degree.png",
     },
 ]
--- src/data/projects.js
+++ src/data/projects.js
@@ -2,35 +2,35 @@ export const projects =[
     {
         title:"Game of Pig",
         description:"This is a simple JavaScript game that rolls two dice and updates the score until a winner is declared.",
-        image:"src/assets/Projects/1game-of-pig.png",
+        image:"/Projects/1game-of-pig.png",
         url:"https://gameofpig.vercel.app/"
     },
 
     {
         title:"Reservations Project",
         description:"This is a theater reservation website, showing occupied, available and selected seats.",
-        image:"src/assets/Projects/2reservations-proyect.png",
+        image:"/Projects/2reservations-proyect.png",
         url:"https://reservations-bay.vercel.app/"
     },
 
     {
         title:"Vacations",
         description:"This is a website that allows you to create a list of vacation destinations or hotels to plan your vacation.",
-        image:"src/assets/Projects/3vacations.png",
+        image:"/Projects/3vacations.png",
         url:"https://vacations.vercel.app/"
     },
 
     {
         title:"Units Converter",
         description:"This is a simple unit converter between miles and kilometers.",
-        image:"src/assets/Projects/4units-converter.png",
+        image:"/Projects/4units-converter.png",
         url:"https://converterunits.vercel.app/"
     },
     
     {
         title:"Pets Coments",
         description:"A page in which it shows the comment left by a customer and rotates between comments every so often.",
-        image:"src/assets/Projects/5pets-coments.png",
+        image:"/Projects/5pets-coments.png",
         url:"https://petscomments.vercel.app/"
     }
    
