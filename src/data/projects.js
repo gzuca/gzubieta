9ae1840a1ba3c8832ed48d9a8c957a1aac60203a
gzuca <gzubica@gmail.com>
2025-09-08 21:01:57 -0500

    New Version with the new projects.

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,4 +1,27 @@
 export const projects =[
+
+    {
+        title:"Virtual Store",
+        description:"This is a React + Vite online store with a clean, responsive interface. It uses useState, useEffect, Context API, and localStorage to manage state and persist data. Product information is fetched from a random API, adding dynamic content and enhancing the browsing experience, showcasing frontend development skills.",
+        image:"/virtual-store.png",
+        url:"https://virtualstore-snowy.vercel.app/"
+    },
+
+    {
+        title:"To-Do List",
+        description:"This is a React web application that lets users create, view, and complete tasks efficiently. It features a clean, responsive interface and includes interactive filter buttons. The project demonstrates the use of React hooks like useState and useEffect, global state management with Context API, and persistent data with localStorage, highlighting frontend development skills.",
+        image:"/To-Do-List.png",
+        url:"https://todolist-htfzbd36f-guillermo-zubietas-projects.vercel.app/"
+    },
+
+    {
+        title:"Simple Count",
+        description:"This is a minimalist React application built with Vite that allows users to increment, decrement, and reset a numerical value. Utilizing React's useState and useEffect hooks, the app provides a responsive and user-friendly interface, demonstrating fundamental frontend development skills.",
+        image:"/Counter.png",
+        url:"https://simplecount.vercel.app/"
+    },
+
+
     {
         title:"Game of Pig",
         description:"This is a simple JavaScript game that rolls two dice and updates the score until a winner is declared.",
